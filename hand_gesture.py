c2989a29a39d84e51997c071c415d0de639299a3
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-05-09 20:11:23 +0530

    week 1:Basic hand detection using MediaPipe

---
+++ hand_gesture.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(min_detection_confidence = 0.7,min_tracking_confidence = 0.5)
+mp_draw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        break
+
+    frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    results = hands.process(frame_rgb)
+
+    if results.multi_hand_landmarks:
+        for landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame,landmarks,mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow('Hand Gesture Recoganition',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
